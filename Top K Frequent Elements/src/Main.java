0c0653ba077fdcffe34b7f58d260b832c55e891b
Marek Polek <marek.pawel.polek@gmail.com>
2024-09-25 11:31:22 +0200

    use right algorythm

--- Top K Frequent Elements/src/Main.java
+++ Top K Frequent Elements/src/Main.java
@@ -1,33 +1,39 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         int [] nums ={1,1,1,2,2,3};
-        topKFrequent(nums,2);
+        int[] ints = topKFrequent(nums, 2);
+        for (int i:ints) {
+            System.out.println(i);
+        }
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> m = new HashMap<>();
-
-        for (int i :nums) {
-            if(m.containsKey(i)){
-                m.put(i,m.get(i)+1);
+        Map<Integer,Integer> map = new HashMap();
+        int [] result = new int[k];
+        List<Integer> bucket [] = new ArrayList[nums.length+1];
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+        for (Map.Entry<Integer,Integer> mapEntry: map.entrySet()) {
+            if(bucket[mapEntry.getValue()]==null){
+                bucket[mapEntry.getValue()]= new ArrayList<>();
             }
-            else{
-                m.put(i,1);
+            bucket[mapEntry.getValue()].add(mapEntry.getKey());
+        }
+        int counter=0;
+        for(int i=bucket.length-1;i>=0 && counter<k;i--){
+            if(bucket[i]!=null){
+                for (Integer integer:bucket[i]) {
+                    result[counter++] = integer;
+
+                }
             }
         }
-        int[] arr =  m.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .map(integerIntegerEntry -> integerIntegerEntry.getKey())
-                .mapToInt(value -> value)
-                .limit(k)
-                .toArray();
-        return arr;
+
+        return result;
     }
 
 }
